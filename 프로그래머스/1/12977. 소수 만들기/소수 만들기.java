bfdfcbb1c56b30dc95c9527bfcbc600bde4b8329
SooHyun Park <147676992+soo59599@users.noreply.github.com>
2025-08-27 09:57:21 +0900

    [level 1] Title: 소수 만들기, Time: 32.98 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        Arrays.sort(nums);
+
+        int N = 0;
+        for(int i = nums.length - 1; i >= nums.length-3; i--){
+            N +=  nums[i];
+        }
+
+        boolean[] isPrime = new boolean[N + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i)
+                    isPrime[j] = false;
+            }
+        }
+
+        for(int i = 2; i <= N; i++){
+            if(isPrime[i]){
+                for(int j = 0 ; j < nums.length -2; j++){
+                    for(int k = j+1; k < nums.length -1;k++){
+                        for(int l = k+1; l < nums.length;l++){
+                            if(i == (nums[j] + nums[k] + nums[l])){
+                                answer += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
