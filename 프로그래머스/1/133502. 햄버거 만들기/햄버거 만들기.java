11d40d96f11220e3f7468b75a9b1225cc8867fc1
SooHyun Park <147676992+soo59599@users.noreply.github.com>
2025-09-14 21:38:41 +0900

    [level 1] Title: 햄버거 만들기, Time: 113.55 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,36 +1,30 @@
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
     class Solution {
         public int solution(int[] ingredient) {
             int answer = 0;
-            Stack<Integer> stack = new Stack<>();
-
-            for(int i = 0; i < ingredient.length; i++) {
+            Deque<Integer> stack = new ArrayDeque<>();
+            for (int i = 0; i < ingredient.length; i++) {
                 stack.push(ingredient[i]);
 
-                // 스택 크기가 4 이상이고, 마지막 4개가 [1,2,3,1] 패턴인지 확인
-                if(stack.size() >= 4) {
-                    // 스택의 맨 위부터 4개 요소 확인
-                    int top1 = stack.get(stack.size() - 1);    // 맨 위 (4번째)
-                    int top2 = stack.get(stack.size() - 2);    // 3번째
-                    int top3 = stack.get(stack.size() - 3);    // 2번째  
-                    int top4 = stack.get(stack.size() - 4);    // 1번째
+                if (stack.size() >= 4) {
+                    int a = stack.pop();
+                    int b = stack.pop();
+                    int c = stack.pop();
+                    int d = stack.pop();
 
-                    // [1,2,3,1] 패턴 확인
-                    if(top4 == 1 && top3 == 2 && top2 == 3 && top1 == 1) {
-                        // 4개 요소 제거 (pop을 4번)
-                        stack.pop();
-                        stack.pop();
-                        stack.pop();
-                        stack.pop();
-
+                    if (a == 1 && b == 3 && c == 2 && d == 1) {
                         answer++;
+                    } else {
+                        stack.push(d);
+                        stack.push(c);
+                        stack.push(b);
+                        stack.push(a);
                     }
                 }
             }
-
             return answer;
         }
     }
