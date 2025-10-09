061d744696cb73dc6836c6e64b7e874c9bbd98f6
SooHyun Park <147676992+soo59599@users.noreply.github.com>
2025-10-10 01:16:37 +0900

    [Silver IV] Title: 카드2, Time: 164 ms, Memory: 45696 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            q.add(i + 1);
+        }
+
+        while (!q.isEmpty()) {
+            if (q.size() == 1) {
+                bw.write(q.poll().toString());
+                break;
+            }
+            q.poll();
+
+            q.add(q.poll());
+
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
